f2677e620a2dabcad6cc74e152151854e6ec473f
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-22 18:33:53 +0200

    griffin: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: sony Model: Griffin Script version: 2.4.0

---
+++ omni_griffin.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from griffin device
+$(call inherit-product, device/sony/griffin/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := griffin
+PRODUCT_NAME := omni_griffin
+PRODUCT_BRAND := Sony
+PRODUCT_MODEL := Griffin
+PRODUCT_MANUFACTURER := sony
